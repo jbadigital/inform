5f928abfa6a0fd7131b415e966ef5a21a7429b27
mafro <github@mafro.net>
2014-07-03 09:20:35 +0000

    SNS loads creds from config file

--- inform/alerts/sns.py
+++ inform/alerts/sns.py
@@ -1,17 +1,41 @@
+import os
+import warnings
+
 import boto.sns
 
 
 class SNSAlert:
     @staticmethod
-    def prepare(region, access_id, secret_key, topic):
+    def prepare(region, topic):
         alert = SNSAlert()
+        try:
+            if alert.load_creds() is False:
+                raise Exception('File not found')
+        except Exception as e:
+            warnings.warn('No SNS credentials found in config/iamcreds.conf: {}'.format(e))
+            return False
+
         alert.conn = boto.sns.connect_to_region(
             region,
-            aws_access_key_id=access_id,
-            aws_secret_access_key=secret_key,
+            aws_access_key_id=alert.access_id,
+            aws_secret_access_key=alert.secret_key,
         )
         alert.topic = topic
         return alert
+
+    def load_creds(self):
+        if not os.path.exists('config/iamcreds.conf'):
+            return False
+
+        # read and parse the IAM config file
+        with open('config/iamcreds.conf', 'r') as f:
+            for line in f.read().splitlines():
+                if 'access_id' in line:
+                    self.access_id = line.split('=')[1]
+                if 'secret_key' in line:
+                    self.secret_key = line.split('=')[1]
+
+        return True
 
     def send(self, message, subject=None):
         print 'SNSAlert: {0}'.format(message)
--- inform/plugins/boomerang.py
+++ inform/plugins/boomerang.py
@@ -33,11 +33,12 @@ class MonitorPlugin(InformBasePlugin):
             timeout=20
         )
         if not results:
-            alert = SNSAlert.prepare(ec2_region, self.access_id, self.secret_key, self.sns_topic)
-            alert.send(
-                'No data retrieved via Salt in {}'.format(ec2_region),
-                'Boomerang Notifications Failure'
-            )
+            alert = SNSAlert.prepare(ec2_region, self.sns_topic)
+            if alert:
+                alert.send(
+                    'No data retrieved via Salt in {}'.format(ec2_region),
+                    'Boomerang Notifications Failure'
+                )
             return {}
 
         for client_ref, data in results[results.keys()[0]].items():
@@ -57,15 +58,16 @@ class MonitorPlugin(InformBasePlugin):
                 output[client_ref][key] = value
 
                 # log everything to graphite
-                if key != "timestamp":
-                    self.log_to_graphite("clients.{}.{}".format(client_ref, key), value)
+                #if key != "timestamp":
+                #    self.log_to_graphite("clients.{}.{}".format(client_ref, key), value)
 
         return output
 
     def alert(self, client_ref, hours):
-        alert = SNSAlert.prepare(ec2_region, self.access_id, self.secret_key, self.sns_topic)
-        alert.send(
-            ('Boomerang sends are zero for the last {} hours for {}'.format(hours, client_ref),
-             '\n\nhttp://monitor.au.basketchaser.com'),
-            'Boomerang Send Warning for {}'.format(client_ref)
-        )
+        alert = SNSAlert.prepare(ec2_region, self.sns_topic)
+        if alert:
+            alert.send(
+                ('Boomerang sends are zero for the last {} hours for {}\n\n'
+                 'http://monitor.au.basketchaser.com').format(hours, client_ref),
+                'Boomerang Send Warning for {}'.format(client_ref)
+            )
